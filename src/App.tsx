04df2d341d1845f9ae7496c4a3333c2f94c4c0bb
Sandeep Yadav <sandeepyadav004343@gmail.com>
2026-03-11 18:11:06 +0530

    App section resolve

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Hero from "./components/Hero"
-import WhoWeAre from "./components/WhoWeAre";
-import WhoWeServe from "./components/WhoWeServe";
-import Services from "./components/Services";
-import ChooseInteger from "./components/ChooseInteger";
-import Banner from "./components/Banner";
+
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import LandingPage from "./components/LandingPage";
